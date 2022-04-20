f53721f2088170d4946fa07ed04dcfb6343d97e9
miftahalulyamaolana17 <iftahmaolana17@gmail.com>
2022-04-20 15:05:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectiftah/Biodata.kt
@@ -0,0 +1,13 @@
+package com.example.projectiftah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Biodata : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+
+    }
+}
